fddb92213d63845881c5e8776c07508cf82a981b
platypushasnohat <58866828+platypushasnohat@users.noreply.github.com>
2025-08-03 22:29:34 -0600

    increment version

--- src/main/java/com/platypushasnohat/shifted_lens/client/models/AnchovyModel.java
+++ src/main/java/com/platypushasnohat/shifted_lens/client/models/AnchovyModel.java
@@ -9,7 +9,11 @@ import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.util.Mth;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
+@OnlyIn(Dist.CLIENT)
+@SuppressWarnings("FieldCanBeLocal, unused")
 public class AnchovyModel<T extends Anchovy> extends HierarchicalModel<T> {
 
 	private final ModelPart Baitfish;
--- src/main/java/com/platypushasnohat/shifted_lens/client/models/SquillModel.java
+++ src/main/java/com/platypushasnohat/shifted_lens/client/models/SquillModel.java
@@ -7,8 +7,11 @@ import net.minecraft.client.model.HierarchicalModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
-import net.minecraft.util.Mth;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
+@OnlyIn(Dist.CLIENT)
+@SuppressWarnings("FieldCanBeLocal, unused")
 public class SquillModel<T extends Squill> extends HierarchicalModel<T> {
 
 	private final ModelPart squill;
